c7f0b43494e439331ea2ae0ba4fc4cea8ddff6e3
Eliza Weisman <eliza@buoyant.io>
2021-05-04 11:22:47 -0700

    console: add TUI app, simple top-style view (#2)
    This branch adds an initial `tui` app skeleton for the `console` CLI,
    including a fairly basic `top(1)`-inspired task list view. There's a lot
    more work to do here, but it works pretty nicely for a quick first pass.
    
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>
    Co-authored-by: David Barsky <me@davidbarsky.com>
    
    ![Screenshot_20210504_110732](https://user-images.githubusercontent.com/2796466/117050047-c3e68300-acc9-11eb-85ee-4aba2827d002.png)

--- console/src/main.rs
+++ console/src/main.rs
@@ -1,3 +1,82 @@
-fn main() {
-    println!("Hello, world!");
+use chrono::Local;
+use console_api::tasks::{tasks_client::TasksClient, TasksRequest};
+use futures::stream::StreamExt;
+use std::io;
+use tokio::select;
+
+use tui::{
+    backend::TermionBackend,
+    layout::{Constraint, Direction, Layout},
+    style::{Color, Modifier, Style},
+    text::{Span, Spans},
+    widgets::{Block, Borders, List, ListItem, Paragraph, Wrap},
+    Terminal,
+};
+
+use tui::backend::CrosstermBackend;
+use tui::style::*;
+use tui::text::*;
+use tui::widgets::*;
+
+mod tasks;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut args = std::env::args();
+    args.next(); // drop the first arg (the name of the binary)
+    let target = args.next().unwrap_or_else(|| {
+        eprintln!("using default address (http://127.0.0.1:6669)");
+        String::from("http://127.0.0.1:6669")
+    });
+
+    let backend = CrosstermBackend::new(io::stdout());
+    let mut terminal = Terminal::new(backend)?;
+    terminal.clear()?;
+
+    let mut client = TasksClient::connect(target.clone()).await?;
+    let request = tonic::Request::new(TasksRequest {});
+    let mut stream = client.watch_tasks(request).await?.into_inner();
+    let mut tasks = tasks::State::default();
+    while let Some(update) = stream.next().await {
+        match update {
+            Ok(update) => {
+                tasks.update(update);
+            }
+            Err(e) => {
+                eprintln!("update stream error: {}", e);
+                return Err(e.into());
+            }
+        }
+
+        terminal.draw(|f| {
+            let chunks = Layout::default()
+                .direction(Direction::Vertical)
+                .margin(0)
+                .constraints([Constraint::Length(2), Constraint::Percentage(95)].as_ref())
+                .split(f.size());
+
+            let header_block = Block::default().title(vec![
+                Span::raw("connected to: "),
+                Span::styled(
+                    target.as_str(),
+                    Style::default().add_modifier(Modifier::BOLD),
+                ),
+            ]);
+
+            let text = vec![Spans::from(vec![
+                Span::styled(
+                    format!("{}", tasks.len()),
+                    Style::default().add_modifier(Modifier::BOLD),
+                ),
+                Span::raw(" tasks"),
+            ])];
+            let header = Paragraph::new(text)
+                .block(header_block)
+                .wrap(Wrap { trim: true });
+            f.render_widget(header, chunks[0]);
+            tasks.render(f, chunks[1]);
+        })?;
+    }
+
+    Ok(())
 }
