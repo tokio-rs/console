495fff10e3b8fa5b5fa1ee5ad00a5c7b14f60686
Eliza Weisman <eliza@buoyant.io>
2021-05-05 12:02:13 -0700

    console: add interactive sorting (#12)
    this branch adds support for sorting tasks in the top tasks view. tasks can
    be sorted by their task ID, total runtime, busy time, idle time, and
    number of polls, and the sorted column can be selected using the left
    and right arrow keys (similar to `top(1)`). the sort direction
    (ascending/descending) can be inverted using the `i` key.
    
    in order to add sorting, it was necessary to add rudimentary plumbing
    for interactivity by setting up `crossterm`'s input-handling utilities
    and handling keypress events. we can add mouse support later.
    
    here's a quick demo of sorting tasks:
    
    [![asciicast](https://asciinema.org/a/X0xi6hzpEPVUaVVVgP26xdATG.svg)][1]
    
    [1]: https://asciinema.org/a/X0xi6hzpEPVUaVVVgP26xdATG
     
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

---
+++ console/src/term.rs
@@ -0,0 +1,47 @@
+pub use color_eyre::eyre::WrapErr;
+use std::io;
+pub use tui::{backend::CrosstermBackend, Terminal};
+
+pub fn init_crossterm() -> color_eyre::Result<(Terminal<CrosstermBackend<io::Stdout>>, OnShutdown)>
+{
+    use crossterm::{
+        event::{DisableMouseCapture, EnableMouseCapture},
+        terminal::{self, EnterAlternateScreen, LeaveAlternateScreen},
+    };
+    terminal::enable_raw_mode().wrap_err("Failed to enable crossterm raw mode")?;
+
+    let mut stdout = std::io::stdout();
+    crossterm::execute!(stdout, EnterAlternateScreen, EnableMouseCapture)
+        .wrap_err("Failed to enable crossterm alternate screen and mouse capture")?;
+    let backend = CrosstermBackend::new(io::stdout());
+    let term = Terminal::new(backend).wrap_err("Failed to create crossterm terminal")?;
+
+    let cleanup = OnShutdown::new(|| {
+        // Be a good terminal citizen...
+        let mut stdout = std::io::stdout();
+        crossterm::execute!(stdout, LeaveAlternateScreen, DisableMouseCapture)
+            .wrap_err("Failed to disable crossterm alternate screen and mouse capture")?;
+        terminal::disable_raw_mode().wrap_err("Failed to enable crossterm raw mode")?;
+        Ok(())
+    });
+
+    Ok((term, cleanup))
+}
+
+pub struct OnShutdown {
+    action: fn() -> color_eyre::Result<()>,
+}
+
+impl OnShutdown {
+    fn new(action: fn() -> color_eyre::Result<()>) -> Self {
+        Self { action }
+    }
+}
+
+impl Drop for OnShutdown {
+    fn drop(&mut self) {
+        if let Err(error) = (self.action)() {
+            tracing::error!(%error, "error running terminal cleanup");
+        }
+    }
+}
