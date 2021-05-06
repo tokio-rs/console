b3e3aa6885fcaaf35954b62578e8394a7644bab8
Eliza Weisman <eliza@buoyant.io>
2021-05-06 10:40:51 -0700

    style(subscriber): break aggregator into fns
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- console-subscriber/src/aggregator.rs
+++ console-subscriber/src/aggregator.rs
@@ -15,10 +15,41 @@ use std::{
 };
 use tracing_core::{span, Metadata};
 pub(crate) struct Aggregator {
+    /// Channel of incoming events emitted by `TaskLayer`s.
     events: mpsc::Receiver<Event>,
+
+    /// New incoming `WatchTasks` RPCs.
     rpcs: mpsc::Receiver<Watch>,
+
+    /// The interval at which new data updates are pushed to clients.
     flush_interval: Duration,
+
+    /// Triggers a flush when the event buffer is approaching capacity.
     flush_capacity: Arc<Flush>,
+
+    // Currently active RPCs streaming task events.
+    watchers: Vec<Watch>,
+
+    /// *All* metadata for task spans and user-defined spans that we care about.
+    ///
+    /// This is sent to new clients as part of the initial state.
+    all_metadata: Vec<proto::register_metadata::NewMetadata>,
+
+    /// *New* metadata that was registered since the last state update.
+    ///
+    /// This is emptied on every state update.
+    new_metadata: Vec<proto::register_metadata::NewMetadata>,
+
+    /// Map of task IDs to task static data.
+    tasks: TaskData<Task>,
+
+    /// Map of task IDs to task stats.
+    stats: TaskData<Stats>,
+
+    /// Task IDs of tasks that completed since the last state update.
+    ///
+    /// This is drained on every state update.
+    completed_spans: Vec<proto::SpanId>,
 }
 
 #[derive(Debug)]
@@ -62,6 +93,14 @@ impl Aggregator {
             rpcs,
             flush_interval,
             events,
+            watchers: Vec::new(),
+            all_metadata: Vec::new(),
+            new_metadata: Vec::new(),
+            tasks: TaskData {
+                data: HashMap::<span::Id, (Task, bool)>::new(),
+            },
+            stats: TaskData::default(),
+            completed_spans: Vec::new(),
         }
     }
 
@@ -71,47 +110,42 @@ impl Aggregator {
 
     pub(crate) async fn run(mut self) {
         let mut flush = tokio::time::interval(self.flush_interval);
-        let mut watches = Vec::new();
-        let mut metadata = Vec::new();
-        let mut new_metadata = Vec::new();
-        let mut tasks = TaskData {
-            data: HashMap::<span::Id, (Task, bool)>::new(),
-        };
-        let mut stats = TaskData::<Stats>::default();
-        let mut completed_spans = Vec::new();
         loop {
             let should_send = tokio::select! {
                 // if the flush interval elapses, flush data to the client
                 _ = flush.tick() => {
                     true
                 }
+
                 // triggered when the event buffer is approaching capacity
                 _ = self.flush_capacity.should_flush.notified() => {
                     self.flush_capacity.triggered.store(false, Release);
                     tracing::debug!("approaching capacity; draining buffer");
                     false
                 }
+
+                // a new client has started watching!
                 subscription = self.rpcs.recv() => {
                     if let Some(subscription) = subscription {
                         tracing::debug!("new subscription");
-                        let new_tasks = tasks.all().map(|(id, task)| {
+                        let new_tasks = self.tasks.all().map(|(id, task)| {
                             task.to_proto(id.clone())
                         }).collect();
                         let now = SystemTime::now();
-                        let stats_update = stats.all().map(|(id, stats)| {
+                        let stats_update = self.stats.all().map(|(id, stats)| {
                             (id.into_u64(), stats.to_proto(now))
                         }).collect();
                         // Send the initial state --- if this fails, the subscription is
                         // already dead.
                         if subscription.update(&proto::tasks::TaskUpdate {
                             new_metadata: Some(proto::RegisterMetadata {
-                                metadata: metadata.clone(),
+                                metadata: self.all_metadata.clone(),
                             }),
                             new_tasks,
                             stats_update,
                             ..Default::default()
                         }) {
-                            watches.push(subscription)
+                            self.watchers.push(subscription)
                         }
                     } else {
                         tracing::debug!("rpc channel closed, terminating");
@@ -132,95 +166,113 @@ impl Aggregator {
             // to be woken when the flush interval has elapsed, or when the
             // channel is almost full.
             while let Some(event) = self.events.recv().now_or_never() {
-                let event = match event {
-                    Some(event) => event,
-                    None => return,
-                };
-
-                // do state update
                 match event {
-                    Event::Metadata(meta) => {
-                        metadata.push(meta.into());
-                        new_metadata.push(meta.into());
+                    Some(event) => self.update_state(event),
+                    // The channel closed, no more events will be emitted...time
+                    // to stop aggregating.
+                    None => {
+                        tracing::debug!("event channel closed; terminating");
+                        return;
                     }
-                    Event::Spawn {
-                        id,
-                        metadata,
-                        at,
-                        fields,
-                    } => {
-                        tasks.insert(
-                            id.clone(),
-                            Task {
-                                metadata,
-                                fields,
-                                // TODO: parents
-                            },
-                        );
-                        stats.insert(
-                            id,
-                            Stats {
-                                polls: 0,
-                                created_at: Some(at),
-                                ..Default::default()
-                            },
-                        );
-                    }
-                    Event::Enter { id, at } => {
-                        let mut stats = stats.update_or_default(id);
-                        if stats.current_polls == 0 {
-                            stats.last_poll = Some(at);
-                            if stats.first_poll == None {
-                                stats.first_poll = Some(at);
-                            }
-                            stats.polls += 1;
-                        }
-                        stats.current_polls += 1;
-                    }
-
-                    Event::Exit { id, at } => {
-                        let mut stats = stats.update_or_default(id);
-                        stats.current_polls -= 1;
-                        if stats.current_polls == 0 {
-                            if let Some(last_poll) = stats.last_poll {
-                                stats.busy_time += at.duration_since(last_poll).unwrap();
-                            }
-                        }
-                    }
-
-                    Event::Close { id, at } => {
-                        stats.update_or_default(id.clone()).closed_at = Some(at);
-                        completed_spans.push(id.into());
-                    }
-                }
+                };
             }
 
             // flush data to clients
             if should_send {
-                let new_metadata = if !new_metadata.is_empty() {
-                    Some(proto::RegisterMetadata {
-                        metadata: mem::replace(&mut new_metadata, Vec::new()),
-                    })
-                } else {
-                    None
-                };
-                let new_tasks = tasks
-                    .since_last_update()
-                    .map(|(id, task)| task.to_proto(id.clone()))
-                    .collect();
-                let now = SystemTime::now();
-                let stats_update = stats
-                    .since_last_update()
-                    .map(|(id, stats)| (id.into_u64(), stats.to_proto(now)))
-                    .collect();
-                let update = proto::tasks::TaskUpdate {
-                    new_metadata,
-                    new_tasks,
-                    stats_update,
-                    completed: mem::replace(&mut completed_spans, Vec::new()),
-                    now: Some(now.into()),
-                };
-                watches.retain(|watch: &Watch| watch.update(&update));
+                self.publish();
+            }
+        }
+    }
+
+    /// Publish the current state to all active watchers.
+    ///
+    /// This drops any watchers which have closed the RPC, or whose update
+    /// channel has filled up.
+    fn publish(&mut self) {
+        let new_metadata = if !self.new_metadata.is_empty() {
+            Some(proto::RegisterMetadata {
+                metadata: mem::replace(&mut self.new_metadata, Vec::new()),
+            })
+        } else {
+            None
+        };
+        let new_tasks = self
+            .tasks
+            .since_last_update()
+            .map(|(id, task)| task.to_proto(id.clone()))
+            .collect();
+        let now = SystemTime::now();
+        let stats_update = self
+            .stats
+            .since_last_update()
+            .map(|(id, stats)| (id.into_u64(), stats.to_proto(now)))
+            .collect();
+        let update = proto::tasks::TaskUpdate {
+            new_metadata,
+            new_tasks,
+            stats_update,
+            completed: mem::replace(&mut self.completed_spans, Vec::new()),
+            now: Some(now.into()),
+        };
+        self.watchers.retain(|watch: &Watch| watch.update(&update));
+    }
+
+    /// Update the current state with data from a single event.
+    fn update_state(&mut self, event: Event) {
+        // do state update
+        match event {
+            Event::Metadata(meta) => {
+                self.all_metadata.push(meta.into());
+                self.new_metadata.push(meta.into());
+            }
+            Event::Spawn {
+                id,
+                metadata,
+                at,
+                fields,
+            } => {
+                self.tasks.insert(
+                    id.clone(),
+                    Task {
+                        metadata,
+                        fields,
+                        // TODO: parents
+                    },
+                );
+                self.stats.insert(
+                    id,
+                    Stats {
+                        polls: 0,
+                        created_at: Some(at),
+                        ..Default::default()
+                    },
+                );
+            }
+            Event::Enter { id, at } => {
+                let mut stats = self.stats.update_or_default(id);
+                if stats.current_polls == 0 {
+                    stats.last_poll = Some(at);
+                    if stats.first_poll == None {
+                        stats.first_poll = Some(at);
+                    }
+                    stats.polls += 1;
+                }
+                stats.current_polls += 1;
+            }
+
+            Event::Exit { id, at } => {
+                let mut stats = self.stats.update_or_default(id);
+                stats.current_polls -= 1;
+                if stats.current_polls == 0 {
+                    if let Some(last_poll) = stats.last_poll {
+                        stats.busy_time += at.duration_since(last_poll).unwrap();
+                    }
+                }
+            }
+
+            Event::Close { id, at } => {
+                self.stats.update_or_default(id.clone()).closed_at = Some(at);
+                self.completed_spans.push(id.into());
             }
         }
     }
