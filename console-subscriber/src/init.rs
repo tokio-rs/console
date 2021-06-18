f2b6de51565b504f83b784f6de8f891fab929578
Jacob Rothstein <hi@jbr.me>
2021-06-18 14:17:45 -0700

    tables!

--- console-subscriber/src/init.rs
+++ console-subscriber/src/init.rs
@@ -12,33 +12,32 @@ type ConsoleSubscriberLayer =
 /// This function represents the easiest way to get started using
 /// tokio-console.
 ///
+/// Please note that this registers the exclusive tracing subscriber
+/// for your application. If you need to add additional layers to this
+/// subscriber, see [`init_with_layer`].
+///
 /// ## Configuration
 ///
 /// Tokio console subscriber is configured with sensible defaults for most
 /// use cases. If you need to tune these parameters, several environmental
 /// configuration variables are available:
 ///
-/// * `TOKIO_CONSOLE_RETENTION_SECS`: The number of seconds to accumulate
-///   completed tracing data. Default: 60s.
-/// * `TOKIO_CONSOLE_BIND`: a HOST:PORT description, such as
-///   localhost:1234 or similar. Default: 127.0.0.1:6669
-/// * `TOKIO_CONSOLE_PUBLISH_INTERVAL_MS`: The number of milliseconds to wait
-///   between sending updates to the console. Default: 1000ms (1s)
-/// * `RUST_LOG`: configure the tracing filter. Default: `tokio=trace`,
-///   and any additional filtering directives will be appended to this
-///   default. See [`EnvFilter`] for further information on the format
-///   for this variable.
+/// | **Environment Variable**            | **Purpose**                                                               | **Default Value** |
+/// |-------------------------------------|---------------------------------------------------------------------------|-------------------|
+/// | `TOKIO_CONSOLE_RETENTION_SECS`      | The number of seconds to accumulate completed tracing data                | 3600s (1h)        |
+/// | `TOKIO_CONSOLE_BIND`                | a HOST:PORT description, such as localhost:1234                           | 127.0.0.1:6669    |
+/// | `TOKIO_CONSOLE_PUBLISH_INTERVAL_MS` | The number of milliseconds to wait between sending updates to the console | 1000ms (1s)       |
+/// | `RUST_LOG`                          | configure the tracing filter. See [`EnvFilter`] for further information   | `tokio=trace`     |
 ///
-/// ## Adding additional layers
-///
-/// To add an additional [`Layer`], see [`init_with_layer`]
 pub fn init() {
     init_inner(|console_subscriber| console_subscriber)
 }
 
-/// Starts the console subscriber server with an additional subscriber
-/// layer, for example a log layer. This interface can be combined
-/// with any of the environment configuration documented at [`init`].
+/// Starts the console subscriber server with an additional layer
+///
+/// This interface can be combined with any of the environment
+/// configuration documented at [`init`]. An example use case might be
+/// composing a log subscriber with the console subscriber.
 pub fn init_with_layer<AdditionalLayer>(additional_layer: AdditionalLayer)
 where
     AdditionalLayer: Layer<ConsoleSubscriberLayer> + Send + Sync + 'static,
@@ -51,21 +50,20 @@ where
     F: FnOnce(ConsoleSubscriberLayer) -> OutputLayer + Send + 'static,
     OutputLayer: Subscriber + Into<Dispatch>,
 {
+    let (layer, server) = TasksLayer::builder().from_default_env().build();
+
+    let filter = EnvFilter::from_default_env().add_directive("tokio=trace".parse().unwrap());
+
+    let console_subscriber = tracing_subscriber::registry()
+        .with(fmt::layer())
+        .with(filter)
+        .with(layer);
+
+    maybe_add_additional_layer(console_subscriber).init();
+
     thread::Builder::new()
         .name("console_subscriber".into())
         .spawn(move || {
-            let (layer, server) = TasksLayer::builder().from_default_env().build();
-
-            let filter =
-                EnvFilter::from_default_env().add_directive("tokio=trace".parse().unwrap());
-
-            let console_subscriber = tracing_subscriber::registry()
-                .with(fmt::layer())
-                .with(filter)
-                .with(layer);
-
-            maybe_add_additional_layer(console_subscriber).init();
-
             let runtime = runtime::Builder::new_current_thread()
                 .enable_io()
                 .enable_time()
