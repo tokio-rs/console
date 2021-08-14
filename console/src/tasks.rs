8d3037e9cff6e811b2fe2764ff7bc794b75915ea
Eliza Weisman <eliza@buoyant.io>
2021-08-14 13:12:51 -0700

    clippy
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- console/src/tasks.rs
+++ console/src/tasks.rs
@@ -581,9 +581,9 @@ impl FieldValue {
 
 impl TaskState {
     pub(crate) fn render(self, styles: &crate::view::Styles) -> &'static str {
-        const RUNNING_UTF8: &'static str = "\u{25B6}";
-        const IDLE_UTF8: &'static str = "\u{23F8}";
-        const COMPLETED_UTF8: &'static str = "\u{23F9}";
+        const RUNNING_UTF8: &str = "\u{25B6}";
+        const IDLE_UTF8: &str = "\u{23F8}";
+        const COMPLETED_UTF8: &str = "\u{23F9}";
         match self {
             Self::Running => styles.if_utf8(RUNNING_UTF8, ">"),
             Self::Idle => styles.if_utf8(IDLE_UTF8, ":"),
