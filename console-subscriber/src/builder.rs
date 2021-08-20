c6fa14dbc69d9ff27629e5cc614cd90d8d4fb5a5
Eliza Weisman <eliza@buoyant.io>
2021-08-20 16:57:46 -0700

    fix order-dependent parsing bugs
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- console-subscriber/src/builder.rs
+++ console-subscriber/src/builder.rs
@@ -164,34 +164,26 @@ fn duration_from_env(var_name: &str) -> Option<Duration> {
 
 fn parse_duration(s: &str) -> Result<Duration, Box<dyn std::error::Error>> {
     let s = s.trim();
-
-    if let Some(s) = s.strip_suffix("ns") {
-        return Ok(Duration::from_nanos(s.trim().parse()?));
-    }
-
-    if let Some(s) = s.strip_suffix("us") {
-        return Ok(Duration::from_micros(s.trim().parse()?));
-    }
-
-    if let Some(s) = s.strip_suffix("ms") {
-        return Ok(Duration::from_millis(s.trim().parse()?));
-    }
-
     if let Some(s) = s
-        .strip_suffix('s')
-        .or_else(|| s.strip_suffix("sec"))
-        .or_else(|| s.strip_suffix("seconds"))
+        .strip_suffix('h')
+        .or_else(|| s.strip_suffix("hour"))
+        .or_else(|| s.strip_suffix("hours"))
     {
         return Ok(s
             .trim()
             .parse::<u64>()
-            .map(Duration::from_secs)
-            .or_else(|_| s.parse::<f64>().map(Duration::from_secs_f64))?);
+            .map(|hours| Duration::from_secs(hours * 60 * 60))
+            .or_else(|_| {
+                s.parse::<f64>()
+                    .map(|hours| Duration::from_secs_f64(hours * 60.0 * 60.0))
+            })?);
     }
 
     if let Some(s) = s
         .strip_suffix('m')
         .or_else(|| s.strip_suffix("min"))
+        .or_else(|| s.strip_suffix("mins"))
+        .or_else(|| s.strip_suffix("minute"))
         .or_else(|| s.strip_suffix("minutes"))
     {
         return Ok(s
@@ -204,15 +196,33 @@ fn parse_duration(s: &str) -> Result<Duration, Box<dyn std::error::Error>> {
             })?);
     }
 
-    if let Some(s) = s.strip_suffix('h').or_else(|| s.strip_suffix("hours")) {
+    if let Some(s) = s.strip_suffix("ms") {
+        return Ok(Duration::from_millis(s.trim().parse()?));
+    }
+
+    if let Some(s) = s.strip_suffix("us") {
+        return Ok(Duration::from_micros(s.trim().parse()?));
+    }
+
+    // Order matters here -- we have to try `ns` for nanoseconds after we try
+    // minutes, because `mins` ends in `ns`.
+    if let Some(s) = s.strip_suffix("ns") {
+        return Ok(Duration::from_nanos(s.trim().parse()?));
+    }
+
+    if let Some(s) = s
+        .strip_suffix("sec")
+        .or_else(|| s.strip_suffix("secs"))
+        .or_else(|| s.strip_suffix("seconds"))
+        // Order matters here -- we have to try `s` for seconds _last_, because
+        // every other plural and subsecond unit also ends in `s`...
+        .or_else(|| s.strip_suffix('s'))
+    {
         return Ok(s
             .trim()
             .parse::<u64>()
-            .map(|hours| Duration::from_secs(hours * 60 * 60))
-            .or_else(|_| {
-                s.parse::<f64>()
-                    .map(|hours| Duration::from_secs_f64(hours * 60.0 * 60.0))
-            })?);
+            .map(Duration::from_secs)
+            .or_else(|_| s.parse::<f64>().map(Duration::from_secs_f64))?);
     }
 
     Err("expected an integer followed by one of {`ns`, `us`, `ms`, `s`, `sec`, `m`, `min`, `h`, `hours`}".into())
