e590f8318cc4ab6346d67f4f4c98a8b4d47c1d58
Eliza Weisman <eliza@buoyant.io>
2021-08-24 10:25:49 -0700

    feat(subscriber): accept durations with units (#93)
    This branch updates the `console-subscriber` env var parsing code to accept
    durations with units. Durations may now be given in any of `ns`, `us`,
    `ms`, `s`, `sec`, `seconds`, `m`, `min`, `minutes`, `h`, or `hours`.
    Durations specified in seconds or a larger unit may also be given as fractional values,
    such as `1.5min`.
    
    As a follow-up, we may also want to allow specifying durations as a
    combination of multiple units, such as `3h15m`.
    
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- console-subscriber/src/builder.rs
+++ console-subscriber/src/builder.rs
@@ -120,19 +120,15 @@ impl Builder {
 
     /// Configures this builder from a standard set of environment variables:
     ///
-    /// | **Environment Variable**            | **Purpose**                                                               | **Default Value** |
-    /// |-------------------------------------|---------------------------------------------------------------------------|-------------------|
-    /// | `TOKIO_CONSOLE_RETENTION_SECS`      | The number of seconds to accumulate completed tracing data                | 3600s (1h)        |
-    /// | `TOKIO_CONSOLE_BIND`                | a HOST:PORT description, such as `localhost:1234`                         | `127.0.0.1:6669`  |
-    /// | `TOKIO_CONSOLE_PUBLISH_INTERVAL_MS` | The number of milliseconds to wait between sending updates to the console | 1000ms (1s)       |
-    /// | `TOKIO_CONSOLE_RECORD_PATH`         | The file path to save a recording                                         | None              |
+    /// | **Environment Variable**         | **Purpose**                                                  | **Default Value** |
+    /// |----------------------------------|--------------------------------------------------------------|-------------------|
+    /// | `TOKIO_CONSOLE_RETENTION`        | The duration of seconds to accumulate completed tracing data | 3600s (1h)        |
+    /// | `TOKIO_CONSOLE_BIND`             | a HOST:PORT description, such as `localhost:1234`            | `127.0.0.1:6669`  |
+    /// | `TOKIO_CONSOLE_PUBLISH_INTERVAL` | The duration to wait between sending updates to the console  | 1000ms (1s)       |
+    /// | `TOKIO_CONSOLE_RECORD_PATH`      | The file path to save a recording                            | None              |
     pub fn with_default_env(mut self) -> Self {
-        if let Ok(retention) = std::env::var("TOKIO_CONSOLE_RETENTION_SECS") {
-            self.retention = Duration::from_secs(
-                retention
-                    .parse()
-                    .expect("TOKIO_CONSOLE_RETENTION_SECS must be an integer"),
-            );
+        if let Some(retention) = duration_from_env("TOKIO_CONSOLE_RETENTION") {
+            self.retention = retention;
         }
 
         if let Ok(bind) = std::env::var("TOKIO_CONSOLE_BIND") {
@@ -143,12 +139,8 @@ impl Builder {
                 .expect("tokio console could not resolve TOKIO_CONSOLE_BIND");
         }
 
-        if let Ok(interval) = std::env::var("TOKIO_CONSOLE_PUBLISH_INTERVAL_MS") {
-            self.publish_interval = Duration::from_millis(
-                interval
-                    .parse()
-                    .expect("TOKIO_CONSOLE_PUBLISH_INTERVAL_MS must be an integer"),
-            );
+        if let Some(interval) = duration_from_env("TOKIO_CONSOLE_PUBLISH_INTERVAL") {
+            self.publish_interval = interval;
         }
 
         if let Ok(path) = std::env::var("TOKIO_CONSOLE_RECORD_PATH") {
@@ -156,5 +148,192 @@ impl Builder {
         }
 
         self
+    }
+}
+
+fn duration_from_env(var_name: &str) -> Option<Duration> {
+    let var = std::env::var(var_name).ok()?;
+    match parse_duration(&var) {
+        Ok(dur) => Some(dur),
+        Err(e) => panic!(
+            "failed to parse a duration from `{}={:?}`: {}",
+            var_name, var, e
+        ),
+    }
+}
+
+fn parse_duration(s: &str) -> Result<Duration, Box<dyn std::error::Error>> {
+    let s = s.trim();
+    if let Some(s) = s
+        .strip_suffix('h')
+        .or_else(|| s.strip_suffix("hour"))
+        .or_else(|| s.strip_suffix("hours"))
+    {
+        let s = s.trim();
+        return Ok(s
+            .parse::<u64>()
+            .map(|hours| Duration::from_secs(hours * 60 * 60))
+            .or_else(|_| {
+                s.parse::<f64>()
+                    .map(|hours| Duration::from_secs_f64(hours * 60.0 * 60.0))
+            })?);
+    }
+
+    if let Some(s) = s
+        .strip_suffix('m')
+        .or_else(|| s.strip_suffix("min"))
+        .or_else(|| s.strip_suffix("mins"))
+        .or_else(|| s.strip_suffix("minute"))
+        .or_else(|| s.strip_suffix("minutes"))
+    {
+        let s = s.trim();
+        return Ok(s
+            .parse::<u64>()
+            .map(|mins| Duration::from_secs(mins * 60))
+            .or_else(|_| {
+                s.parse::<f64>()
+                    .map(|mins| Duration::from_secs_f64(mins * 60.0))
+            })?);
+    }
+
+    if let Some(s) = s.strip_suffix("ms") {
+        return Ok(Duration::from_millis(s.trim().parse()?));
+    }
+
+    if let Some(s) = s.strip_suffix("us") {
+        return Ok(Duration::from_micros(s.trim().parse()?));
+    }
+
+    // Order matters here -- we have to try `ns` for nanoseconds after we try
+    // minutes, because `mins` ends in `ns`.
+    if let Some(s) = s.strip_suffix("ns") {
+        return Ok(Duration::from_nanos(s.trim().parse()?));
+    }
+
+    if let Some(s) = s
+        .strip_suffix("sec")
+        .or_else(|| s.strip_suffix("secs"))
+        .or_else(|| s.strip_suffix("seconds"))
+        // Order matters here -- we have to try `s` for seconds _last_, because
+        // every other plural and subsecond unit also ends in `s`...
+        .or_else(|| s.strip_suffix('s'))
+    {
+        let s = s.trim();
+        return Ok(s
+            .parse::<u64>()
+            .map(Duration::from_secs)
+            .or_else(|_| s.parse::<f64>().map(Duration::from_secs_f64))?);
+    }
+
+    Err("expected an integer followed by one of {`ns`, `us`, `ms`, `s`, `sec`, `m`, `min`, `h`, `hours`}".into())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn test_parse_durations(expected: Duration, inputs: &[&str]) {
+        for input in inputs {
+            println!("trying: parse_duration({:?}) -> {:?}", input, expected);
+            match parse_duration(input) {
+                Err(e) => panic!(
+                    "parse_duration({:?}) -> {} (expected {:?})",
+                    input, e, expected
+                ),
+                Ok(dur) => assert_eq!(
+                    dur, expected,
+                    "parse_duration({:?}) -> {:?} (expected {:?})",
+                    input, dur, expected
+                ),
+            }
+        }
+    }
+
+    #[test]
+    fn parse_hours() {
+        test_parse_durations(
+            Duration::from_secs(3 * 60 * 60),
+            &["3h", "3 h", " 3 h", "3 hours", "3hours"],
+        )
+    }
+
+    #[test]
+    fn parse_mins() {
+        test_parse_durations(
+            Duration::from_secs(10 * 60),
+            &[
+                "10m",
+                "10 m",
+                "10 m",
+                "10 minutes",
+                "10minutes",
+                "  10 minutes",
+                "10 min",
+                " 10 min",
+                "10min",
+            ],
+        )
+    }
+
+    #[test]
+    fn parse_secs() {
+        test_parse_durations(
+            Duration::from_secs(10),
+            &[
+                "10s",
+                "10 s",
+                "10 s",
+                "10 seconds",
+                "10seconds",
+                "  10 seconds",
+                "10 sec",
+                " 10 sec",
+                "10sec",
+            ],
+        )
+    }
+
+    #[test]
+    fn parse_fractional_hours() {
+        test_parse_durations(
+            Duration::from_millis(1500 * 60 * 60),
+            &["1.5h", "1.5 h", " 1.5 h", "1.5 hours", "1.5hours"],
+        )
+    }
+
+    #[test]
+    fn parse_fractional_mins() {
+        test_parse_durations(
+            Duration::from_millis(1500 * 60),
+            &[
+                "1.5m",
+                "1.5 m",
+                "1.5 m",
+                "1.5 minutes",
+                "1.5 minutes",
+                "  1.5 minutes",
+                "1.5 min",
+                " 1.5 min",
+                "1.5min",
+            ],
+        )
+    }
+
+    #[test]
+    fn parse_fractional_secs() {
+        test_parse_durations(
+            Duration::from_millis(1500),
+            &[
+                "1.5s",
+                "1.5 s",
+                "1.5 s",
+                "1.5 seconds",
+                "1.5 seconds",
+                "  1.5 seconds",
+                "1.5 sec",
+                " 1.5 sec",
+                "1.5sec",
+            ],
+        )
     }
 }
