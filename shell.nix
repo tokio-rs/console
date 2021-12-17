4256946f03b3985c85918bb187f3fe2e79d84e90
Josh Robson Chase <josh@robsonchase.com>
2021-12-17 12:13:27 -0500

    chore(nix): refactor for flake support and add package build
    Pins nixpkgs to a known working version for the flake.
    Things should be unchanged for non-flake-users.

--- shell.nix
+++ shell.nix
@@ -1,12 +1,37 @@
 scope@{ pkgs ? import <nixpkgs> { } }:
+with pkgs;
+let
+  tokio-console = import ./default.nix { inherit pkgs; };
 
-let env = (import ./default.nix scope);
+  haveGlibcLocales = pkgs.glibcLocales != null && stdenv.hostPlatform.libc == "glibc";
+  glibcLocales-utf8 = (glibcLocales.override { locales = [ "en_US.UTF-8" ]; });
 
-in with pkgs;
+  env = buildEnv {
+    name = "console-env";
+    paths = [
+      direnv
+      binutils
+      stdenv
+      bashInteractive
+      docker
+      cacert
+      gcc
+      cmake
+      pkg-config
+      openssl
+    ]
+    ++ lib.optional haveGlibcLocales glibcLocales-utf8
+    ++ lib.optionals stdenv.isDarwin [ libiconv ]
+    ++ tokio-console.buildInputs
+    ++ tokio-console.nativeBuildInputs;
+  };
+in
 mkShell {
+  buildInputs = [ env ];
+
   PROTOC = "${protobuf}/bin/protoc";
   PROTOC_INCLUDE = "${protobuf}/include";
-  LOCALE_ARCHIVE = "${glibcLocales}/lib/locale/locale-archive";
+  LOCALE_ARCHIVE = lib.optionalString haveGlibcLocales "${glibcLocales-utf8}/lib/locale/locale-archive";
   LC_ALL = "en_US.UTF-8";
   OPENSSL_DIR = "${openssl.dev}";
   OPENSSL_LIB_DIR = "${openssl.out}/lib";
@@ -14,6 +39,5 @@ mkShell {
   GIT_SSL_CAINFO = "${cacert}/etc/ssl/certs/ca-bundle.crt";
   CURL_CA_BUNDLE = "${cacert}/etc/ca-bundle.crt";
   CARGO_TERM_COLOR = "always";
-  RUST_BACKTRACE = "1";
-  buildInputs = [ (import ./default.nix { inherit pkgs; }) ];
+  RUST_BACKTRACE = "full";
 }
---
+++ tokio-console.nix
@@ -0,0 +1,56 @@
+{ lib
+, protobuf
+, rustPlatform
+, nix-gitignore
+}:
+let
+  inherit (nix-gitignore) gitignoreFilterPure withGitignoreFile;
+  # Workaround for the builtins.filterSource issue mentioned in
+  # https://nixos.org/manual/nix/unstable/expressions/builtins.html
+  # Since this might be built from a flake, the source path may be a store path,
+  # so we need to provide our own version of gitignoreSource that avoids
+  # builtins.filterSource in favor of builtins.path.
+  gitignoreSource = patterns: path:
+    builtins.path {
+      filter = gitignoreFilterPure (_: _: true) (withGitignoreFile patterns path) path;
+      path = path;
+      name = "src";
+    };
+
+  cargoTOML = lib.importTOML ./console/Cargo.toml;
+
+  # Ignore some extra things that don't factor into the main build to help with
+  # caching.
+  extraIgnores = ''
+    /.envrc
+    /*.nix
+    /flake.*
+    /netlify.toml
+    /.github
+    /assets
+    /*.md
+    /.gitignore
+    /LICENSE
+  '';
+
+  src = gitignoreSource extraIgnores ./.;
+in
+rustPlatform.buildRustPackage rec {
+  pname = cargoTOML.package.name;
+  version = cargoTOML.package.version;
+
+  nativeBuildInputs = [
+    protobuf
+  ];
+
+  inherit src;
+
+  cargoLock = {
+    lockFile = "${src}/Cargo.lock";
+  };
+
+  meta = {
+    inherit (cargoTOML.package) description homepage license;
+    maintainers = cargoTOML.package.authors;
+  };
+}
