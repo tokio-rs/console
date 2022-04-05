4c966b339c8a88bebe94df114dfb224da078db19
nrskt <norisuke_takafuji@caddi.jp>
2022-04-06 00:30:21 +0900

    feat(console): support config file
    - fix command line parser
    - fix config file parser

--- tokio-console/src/config.rs
+++ tokio-console/src/config.rs
@@ -1,5 +1,8 @@
 use crate::view::Palette;
 use clap::{ArgGroup, Parser as Clap, ValueHint};
+use serde::Deserialize;
+use std::fs;
+use std::ops::Not;
 use std::process::Command;
 use std::str::FromStr;
 use std::time::Duration;
@@ -71,15 +74,15 @@ struct RetainFor(Option<Duration>);
 pub struct ViewOptions {
     /// Disable ANSI colors entirely.
     #[clap(name = "no-colors", long = "no-colors")]
-    no_colors: bool,
+    no_colors: Option<bool>,
 
     /// Overrides the terminal's default language.
-    #[clap(long = "lang", env = "LANG", default_value = "en_us.UTF-8")]
-    lang: String,
+    #[clap(long = "lang", env = "LANG")]
+    lang: Option<String>,
 
     /// Explicitly use only ASCII characters.
     #[clap(long = "ascii-only")]
-    ascii_only: bool,
+    ascii_only: Option<bool>,
 
     /// Overrides the value of the `COLORTERM` environment variable.
     ///
@@ -110,17 +113,57 @@ pub struct ViewOptions {
 #[derive(Clap, Debug, Copy, Clone)]
 pub struct ColorToggles {
     /// Disable color-coding for duration units.
-    #[clap(long = "no-duration-colors", parse(from_flag = std::ops::Not::not), group = "colors")]
-    pub(crate) color_durations: bool,
+    #[clap(long = "no-duration-colors", group = "colors")]
+    color_durations: Option<bool>,
 
     /// Disable color-coding for terminated tasks.
-    #[clap(long = "no-terminated-colors", parse(from_flag = std::ops::Not::not), group = "colors")]
-    pub(crate) color_terminated: bool,
+    #[clap(long = "no-terminated-colors", group = "colors")]
+    color_terminated: Option<bool>,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct ConfigFile {
+    charset: Option<CharsetConfig>,
+    colors: Option<ColorsConfig>,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct CharsetConfig {
+    lang: String,
+    ascii_only: bool,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct ColorsConfig {
+    enabled: bool,
+    truecolor: bool,
+    palette: Palette,
+    enable: ColorsEnable,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct ColorsEnable {
+    durations: bool,
+    terminated: bool,
 }
 
 // === impl Config ===
 
 impl Config {
+    pub fn from_config() -> Self {
+        let base_view_options = ConfigFile::from_config().map(|config| config.into_view_options());
+        let mut config = Self::parse();
+        let view_options = match base_view_options {
+            None => config.view_options,
+            Some(mut base) => {
+                merge_view_options(&mut base, config.view_options);
+                base
+            }
+        };
+        config.view_options = view_options;
+        config
+    }
+
     pub fn trace_init(&mut self) -> color_eyre::Result<()> {
         let filter = std::mem::take(&mut self.env_filter);
         use tracing_subscriber::prelude::*;
@@ -163,11 +206,43 @@ impl Config {
     }
 }
 
+fn merge_view_options(base: &mut ViewOptions, command_line: ViewOptions) {
+    if command_line.no_colors.is_some() {
+        base.no_colors = command_line.no_colors;
+    }
+
+    if command_line.lang.is_some() {
+        base.lang = command_line.lang;
+    }
+
+    if command_line.ascii_only.is_some() {
+        base.ascii_only = command_line.ascii_only;
+    }
+
+    if command_line.truecolor.is_some() {
+        base.truecolor = command_line.truecolor;
+    }
+
+    if command_line.palette.is_some() {
+        base.palette = command_line.palette;
+    }
+
+    if command_line.toggles.color_durations.is_some() {
+        base.toggles.color_durations = command_line.toggles.color_durations;
+    }
+
+    if command_line.toggles.color_terminated.is_some() {
+        base.toggles.color_terminated = command_line.toggles.color_terminated;
+    }
+}
+
 // === impl ViewOptions ===
 
 impl ViewOptions {
     pub fn is_utf8(&self) -> bool {
-        self.lang.ends_with("UTF-8") && !self.ascii_only
+        let lang = self.lang.clone().unwrap_or_default();
+        let ascii_only = self.ascii_only.unwrap_or(true);
+        lang.ends_with("UTF-8") && !ascii_only
     }
 
     /// Determines the color palette to use.
@@ -179,7 +254,7 @@ impl ViewOptions {
     /// - Checking the `terminfo` database via `tput`
     pub(crate) fn determine_palette(&self) -> Palette {
         // Did the user explicitly disable colors?
-        if self.no_colors {
+        if self.no_colors.unwrap_or(true) {
             tracing::debug!("colors explicitly disabled by `--no-colors`");
             return Palette::NoColors;
         }
@@ -235,6 +310,70 @@ impl FromStr for RetainFor {
             _ => s
                 .parse::<humantime::Duration>()
                 .map(|duration| RetainFor(Some(duration.into()))),
+        }
+    }
+}
+
+// === impl ColorToggles ===
+
+impl ColorToggles {
+    pub fn color_durations(&self) -> bool {
+        self.color_durations.map(std::ops::Not::not).unwrap_or(true)
+    }
+
+    pub fn color_terminated(&self) -> bool {
+        self.color_durations.map(std::ops::Not::not).unwrap_or(true)
+    }
+}
+
+// === impl ColorToggles ===
+
+impl ConfigFile {
+    fn from_config() -> Option<Self> {
+        let mut base = dirs::config_dir();
+        if let Some(path) = base.as_mut() {
+            path.push("tokio-console/console.toml");
+        }
+        let base = base.and_then(|path| fs::read_to_string(path).ok());
+        let base_file: Option<ConfigFile> = base.and_then(|raw| toml::from_str(&raw).ok());
+
+        let current = fs::read_to_string("./console.toml").ok();
+        let current_file: Option<ConfigFile> = current.and_then(|raw| toml::from_str(&raw).ok());
+        merge_config_file(base_file, current_file)
+    }
+
+    fn into_view_options(self) -> ViewOptions {
+        ViewOptions {
+            no_colors: self.colors.as_ref().map(|config| Not::not(config.enabled)),
+            lang: self.charset.as_ref().map(|config| config.lang.to_string()),
+            ascii_only: self.charset.as_ref().map(|config| config.ascii_only),
+            truecolor: self.colors.as_ref().map(|config| config.truecolor),
+            palette: self.colors.as_ref().map(|config| config.palette),
+            toggles: ColorToggles {
+                color_durations: self
+                    .colors
+                    .as_ref()
+                    .map(|config| Not::not(config.enable.durations)),
+                color_terminated: self.colors.map(|config| Not::not(config.enable.terminated)),
+            },
+        }
+    }
+}
+
+fn merge_config_file(before: Option<ConfigFile>, after: Option<ConfigFile>) -> Option<ConfigFile> {
+    match (before, after) {
+        (None, None) => None,
+        (before @ Some(_), None) => before,
+        (None, after @ Some(_)) => after,
+        (Some(mut before), Some(after)) => {
+            let ConfigFile { charset, colors } = after;
+            if let Some(charset) = charset {
+                before.charset = Some(charset)
+            }
+            if let Some(colors) = colors {
+                before.colors = Some(colors)
+            }
+            Some(before)
         }
     }
 }
