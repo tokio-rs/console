ef39b9a6419227d10e7a4d299ca95673ea200944
Eliza Weisman <eliza@buoyant.io>
2022-04-30 10:02:14 -0700

    fix(console): always log to a file instead of `stderr` (#340)
    ## Motivation
    
    Currently, the `tokio-console` CLI includes `tracing` support for
    internal debugging. By default, the logs are written to `stderr`, with
    the expectations that users will redirect them to a file if logging is
    enabled. 
    
    This isn't great: it means that if a `RUST_LOG` env var is set, but the
    user didn't intend to enable the console's logging, logs will be written
    to the terminal *while displaying the console TUI*. This results in the
    TUI itself getting all messed up (see #339).
    
    If support for `systemd-journald` is enabled, logs are instead written
    to the system journal, to avoid having to use this redirection dance. We
    should probably change our `tracing` configuration so logs are never
    written to stderr/stdout, regardless of whether `journald` is enabled.
    
    ## Solution
    
    This branch changes the console so that it logs to a file when
    `journald` support is not enabled, rather than to stderr. The log file
    location can be overridden from the CLI and config file, and defaults to
    `/tmp/tokio-console/logs`. This should fix logging messing up the TUI.
    
    Fixes #339
    
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- tokio-console/src/config.rs
+++ tokio-console/src/config.rs
@@ -31,13 +31,21 @@ pub struct Config {
 
     /// Log level filter for the console's internal diagnostics.
     ///
-    /// The console will log to stderr if a log level filter is provided. Since
-    /// the console application runs interactively, stderr should generally be
-    /// redirected to a file to avoid interfering with the console's text output.
+    /// Logs are written to a new file at the path given by the `--log-dir`
+    /// argument (or its default value), or to the system journal if
+    /// `systemd-journald` support is enabled.
+    ///
+    /// If this is set to 'off' or is not set, no logs will be written.
     ///
     /// [default: off]
     #[clap(long = "log", env = "RUST_LOG")]
     pub(crate) env_filter: Option<tracing_subscriber::EnvFilter>,
+
+    /// Path to a directory to write the console's internal logs to.
+    ///
+    /// [default: /tmp/tokio-console/logs]
+    #[clap(long = "log-dir", value_hint = ValueHint::DirPath)]
+    pub(crate) log_directory: Option<PathBuf>,
 
     #[clap(flatten)]
     pub(crate) view_options: ViewOptions,
@@ -179,6 +187,7 @@ pub struct ColorToggles {
 struct ConfigFile {
     default_target_addr: Option<String>,
     log: Option<String>,
+    log_directory: Option<PathBuf>,
     retention: Option<RetainFor>,
     charset: Option<CharsetConfig>,
     colors: Option<ColorsConfig>,
@@ -228,8 +237,13 @@ impl Config {
     }
 
     pub fn trace_init(&mut self) -> color_eyre::Result<()> {
-        let filter = std::mem::take(&mut self.env_filter);
         use tracing_subscriber::prelude::*;
+        let filter = match self.env_filter.take() {
+            // if logging is totally disabled, don't bother even constructing
+            // the subscriber
+            None => return Ok(()),
+            Some(filter) => filter,
+        };
 
         // If we're on a Linux distro with journald, try logging to the system
         // journal so we don't interfere with text output.
@@ -243,13 +257,41 @@ impl Config {
         #[cfg(not(all(feature = "tracing-journald", target_os = "linux")))]
         let should_fmt = true;
 
-        // Otherwise, log to stderr and rely on the user redirecting output.
+        // Otherwise, log to a file.
         let fmt = if should_fmt {
-            Some(
-                tracing_subscriber::fmt::layer()
-                    .with_writer(std::io::stderr)
-                    .with_ansi(atty::is(atty::Stream::Stderr)),
-            )
+            let dir = self
+                .log_directory
+                .take()
+                .unwrap_or_else(default_log_directory);
+
+            // first ensure that the log directory exists
+            fs::create_dir_all(&dir)
+                .with_context(|| format!("creating log directory '{}'", dir.display()))?;
+            color_eyre::eyre::ensure!(
+                dir.is_dir(),
+                "log directory path '{}' is not a directory",
+                dir.display()
+            );
+
+            // now, open a log file
+            let now = std::time::SystemTime::now();
+            // format the current time in a way that's appropriate for a
+            // filename (strip the `:` character, as it is an invalid filename
+            // char on windows)
+            let filename =
+                format!("{}.log", humantime::format_rfc3339_seconds(now)).replace(':', "");
+            let path = dir.join(filename);
+            let file = fs::File::options()
+                .create_new(true)
+                .write(true)
+                .open(&path)
+                .with_context(|| format!("creating log file '{}'", path.display()))?;
+
+            // finally, construct a `fmt` layer to write to that log file
+            let fmt = tracing_subscriber::fmt::layer()
+                .with_ansi(false)
+                .with_writer(file);
+            Some(fmt)
         } else {
             None
         };
@@ -283,6 +325,7 @@ impl Config {
 
     fn merge_with(self, other: Self) -> Self {
         Self {
+            log_directory: other.log_directory.or(self.log_directory),
             target_addr: other.target_addr.or(self.target_addr),
             env_filter: other.env_filter.or(self.env_filter),
             retain_for: other.retain_for.or(self.retain_for),
@@ -297,6 +340,7 @@ impl Default for Config {
         Self {
             target_addr: Some(default_target_addr()),
             env_filter: Some(tracing_subscriber::EnvFilter::new("off")),
+            log_directory: Some(default_log_directory()),
             retain_for: Some(RetainFor(Some(Duration::from_secs(6)))),
             view_options: ViewOptions::default(),
             subcmd: None,
@@ -308,6 +352,10 @@ fn default_target_addr() -> Uri {
     "http://127.0.0.1:6669"
         .parse::<Uri>()
         .expect("default target address should be a valid URI")
+}
+
+fn default_log_directory() -> PathBuf {
+    ["/", "tmp", "tokio-console", "logs"].iter().collect()
 }
 
 // === impl ViewOptions ===
@@ -473,9 +521,14 @@ impl ConfigFile {
     }
 
     fn env_filter(&self) -> color_eyre::Result<Option<tracing_subscriber::EnvFilter>> {
-        let env_filter = self
-            .log
-            .as_ref()
+        let filter_str = self.log.as_deref();
+
+        // If logging is totally disabled, may as well bail completely.
+        if filter_str == Some("off") {
+            return Ok(None);
+        }
+
+        let env_filter = filter_str
             .map(|directive| directive.parse::<tracing_subscriber::EnvFilter>())
             .transpose()
             .wrap_err_with(|| format!("failed to parse log filter {:?}", self.log))?;
@@ -510,6 +563,7 @@ impl From<Config> for ConfigFile {
         Self {
             default_target_addr: config.target_addr.map(|addr| addr.to_string()),
             log: config.env_filter.map(|filter| filter.to_string()),
+            log_directory: config.log_directory,
             retention: config.retain_for,
             charset: Some(CharsetConfig {
                 lang: config.view_options.lang,
@@ -528,10 +582,11 @@ impl From<Config> for ConfigFile {
 impl TryFrom<ConfigFile> for Config {
     type Error = color_eyre::eyre::Error;
 
-    fn try_from(value: ConfigFile) -> Result<Self, Self::Error> {
+    fn try_from(mut value: ConfigFile) -> Result<Self, Self::Error> {
         Ok(Config {
             target_addr: value.target_addr()?,
             env_filter: value.env_filter()?,
+            log_directory: value.log_directory.take(),
             retain_for: value.retain_for(),
             view_options: ViewOptions {
                 no_colors: value.no_colors(),
@@ -631,6 +686,8 @@ mod tests {
     }
 
     #[test]
+    // The example output includes paths, so skip this test on windows. :/
+    #[cfg_attr(windows, ignore)]
     fn toml_example_changed() {
         // Override env vars that may effect the defaults.
         clobber_env_vars();
