3c40e88eba373a43ae3786351a9a61876af1d138
Hayden Stainsby <hds@caffeineconcepts.com>
2023-06-16 15:25:24 +0200

    use duplex stream to connect client and server
    Connecting the gRPC client and server via TCP for the test means that
    the port must be randomized so that multiple tests can run at the same
    time. This creates the possibility for flaky tests if you get unlucky
    with the randomization - or requires the introduction of retry logic.
    
    Instead, we use a duplex stream from Tokio as per the Tonic `mock`
    example:
    https://github.com/hyperium/tonic/blob/1934825ff52bff26bb88b709aee9ac73d3ea51c0/examples/src/mock/mock.rs

--- console-subscriber/tests/wake.rs
+++ console-subscriber/tests/wake.rs
@@ -2,16 +2,20 @@ use std::{thread, time::Duration};
 
 use console_api::instrument::{instrument_client::InstrumentClient, InstrumentRequest};
 use futures::stream::StreamExt;
-use tokio::{sync::oneshot, task, time::sleep};
+use tokio::{sync::broadcast, task};
+use tonic::transport::{Endpoint, Server, Uri};
+use tower::service_fn;
 use tracing_subscriber::prelude::*;
 
 #[test]
 fn self_wake() {
+    let (client_stream, server_stream) = tokio::io::duplex(1024);
+
     let (console_layer, server) = console_subscriber::ConsoleLayer::builder().build();
 
     let registry = tracing_subscriber::registry().with(console_layer);
 
-    let (finish_tx, finish_rx) = oneshot::channel::<()>();
+    let (finish_tx, mut finish_rx) = broadcast::channel(1);
 
     let join_handle = thread::Builder::new()
         .name("console_subscriber".into())
@@ -24,41 +28,57 @@ fn self_wake() {
                 .build()
                 .expect("console subscriber runtime initialization failed");
 
+            let mut console_server_finish_rx = finish_tx.subscribe();
             runtime
                 .block_on(async move {
                     task::Builder::new()
                         .name("console_server")
                         .spawn(async move {
-                            server
-                                .serve()
+                            let (service, aggregate) = server.into_parts();
+                            Server::builder()
+                                .add_service(service)
+                                .serve_with_incoming(futures::stream::iter(vec![Ok::<_, std::io::Error>(server_stream)]))
                                 .await
-                                .expect("console subscriber server failed")
+                                .expect("console subscriber failed.");
+                            println!("Waiting for finish signal");
+                            match console_server_finish_rx.recv().await {
+                                Ok(_) => println!("Getting ready to drop the aggregate handle."),
+                                Err(err) => println!("Error waiting for finish signal: {err:?}"),
+                            }
+                            drop(aggregate);
                         })
                         .unwrap();
 
-                    let expect = task::Builder::new().name("expect").spawn(async {
-                        tokio::time::sleep(std::time::Duration::from_millis(200)).await;
+                    let expect = task::Builder::new().name("expect").spawn(async move {
+                        tokio::time::sleep(Duration::from_millis(200)).await;
 
-                        let target = "http://127.0.0.1:6669".to_owned();
-                        let mut client = InstrumentClient::connect(target).await.unwrap();
+                        let mut client_stream = Some(client_stream);
+                        let channel = Endpoint::try_from("http://[::]:6669")
+                        .expect("Could not create endpoint")
+                        .connect_with_connector(service_fn(move |_: Uri| {
+                            let client = client_stream.take();
 
+                            async move {
+                                if let Some(client) = client {
+                                    Ok(client)
+                                } else {
+                                    Err(std::io::Error::new(
+                                        std::io::ErrorKind::Other,
+                                        "Client already taken",
+                                    ))
+                                }
+                            }
+                        }))
+                        .await
+                        .expect("Could not create client");
 
-                        let mut fail_count = 0;
+                        let mut client = InstrumentClient::new(channel);
+
                         let mut stream = loop {
                             let request = tonic::Request::new(InstrumentRequest {});
                             match client.watch_updates(request).await {
                                 Ok(stream) => break stream.into_inner(),
-                                Err(err) => {
-                                    if fail_count < 5 {
-                                        fail_count += 1;
-                                        println!(
-                                        "Could not connect ({fail_count}), will try again: {err}"
-                                    );
-                                        sleep(Duration::from_millis(fail_count * 100)).await;
-                                    } else {
-                                        panic!("Client cannot connect to watch updates: {err}");
-                                    }
-                                }
+                                Err(err) => panic!("Client cannot connect to watch updates: {err}"),
                             }
                         };
 
@@ -66,7 +86,7 @@ fn self_wake() {
                         while let Some(update) = stream.next().await {
                             match update {
                                 Ok(update) => {
-                                    println!("UPDATE {}: {:#?}\n", i, update.task_update);
+                                    println!("UPDATE {}: {:#?}", i, update.task_update);
                                     if let Some(task_update) = update.task_update {
                                         println!(
                                             "UPDATE: new task count: {}, update count: {}, dropped count: {}",
@@ -111,7 +131,7 @@ fn self_wake() {
             .unwrap();
         runtime.block_on(async {
             println!("Before await finish...");
-            match finish_rx.await {
+            match finish_rx.recv().await {
                 Ok(_) => println!("finish message received."),
                 Err(err) => println!("finish message could not be received: {err}"),
             }
