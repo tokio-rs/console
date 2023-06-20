20483939082d6db41764e78cc95d1593c695d6bb
Hayden Stainsby <hds@caffeineconcepts.com>
2023-06-20 16:22:33 +0200

    added expectations
    First (sort of) failing test. Basically it all fits together.
    
    Now for a lot of serious refactoring so that it makes sense and I can
    actually write the real test.

--- console-subscriber/tests/wake.rs
+++ console-subscriber/tests/wake.rs
@@ -29,6 +29,91 @@ impl ActualTask {
     }
 }
 
+#[derive(Clone, Debug)]
+struct ExpectedTask {
+    match_name: Option<String>,
+
+    expect_wakes: Option<u64>,
+    expect_self_wakes: Option<u64>,
+}
+
+impl Default for ExpectedTask {
+    fn default() -> Self {
+        Self {
+            match_name: Default::default(),
+            expect_wakes: Default::default(),
+            expect_self_wakes: Default::default(),
+        }
+    }
+}
+
+impl ExpectedTask {
+    fn name(&self) -> &str {
+        match &self.match_name {
+            Some(name) => name,
+            None => "",
+        }
+    }
+
+    fn matches_actual_task(&self, actual_task: &ActualTask) -> bool {
+        if let Some(match_name) = &self.match_name {
+            if Some(match_name) == actual_task.name.as_ref() {
+                return true;
+            }
+        }
+
+        false
+    }
+
+    fn validate_actual_task(&self, actual_task: &ActualTask) -> bool {
+        let mut no_expectations = true;
+        if let Some(expected_wakes) = self.expect_wakes {
+            no_expectations = false;
+            if expected_wakes != actual_task.wakes {
+                println!(
+                    "error: Task<name={name}>: expected `wakes` to be {expected_wakes}, but actual was {actual_wakes}",
+                    name = self.name(),
+                    actual_wakes = actual_task.wakes);
+                return false;
+            }
+        }
+
+        if let Some(expected_self_wakes) = self.expect_self_wakes {
+            no_expectations = false;
+            if expected_self_wakes != actual_task.self_wakes {
+                println!(
+                    "error: Task<name={name}>: expected `self_wakes` to be {expected_self_wakes}, but actual was {actual_self_wakes}",
+                    name = self.name(),
+                    actual_self_wakes = actual_task.self_wakes);
+                return false;
+            }
+        }
+
+        if no_expectations {
+            println!(
+                "warn: Task<name={name}>: validated, but no expectations found. Did you forget to set some?",
+                name = self.name());
+        }
+
+        true
+    }
+
+    fn match_name(mut self, name: String) -> Self {
+        self.match_name = Some(name);
+        self
+    }
+
+    fn expect_wakes(mut self, wakes: u64) -> Self {
+        self.expect_wakes = Some(wakes);
+        self
+    }
+
+    fn expect_self_wakes(mut self, self_wakes: u64) -> Self {
+        self.expect_self_wakes = Some(self_wakes);
+        self
+    }
+}
+
 #[test]
 fn self_wake() {
     let (client_stream, server_stream) = tokio::io::duplex(1024);
@@ -106,6 +191,9 @@ fn self_wake() {
 
                         let mut tasks = HashMap::new();
 
+                        let expected_task = ExpectedTask::default().match_name("mog".into()).expect_wakes(1).expect_self_wakes(0);
+                        let expected_tasks = vec![expected_task];
+
                         let mut i: usize = 0;
                         while let Some(update) = stream.next().await {
                             match update {
@@ -174,7 +262,31 @@ fn self_wake() {
                             }
                         }
 
-                        println!("{tasks:#?}");
+                        let mut test_passes = None;
+                        for (_, actual) in &tasks {
+                            for expected in &expected_tasks {
+                                if expected.matches_actual_task(actual) {
+                                    if expected.validate_actual_task(actual) {
+                                        test_passes = Some(true);
+                                    } else {
+                                        test_passes = Some(false);
+                                    }
+                                }
+
+                                match test_passes {
+                                    Some(false) => break,
+                                    _ => {},
+                                }
+                            }
+                        }
+
+                        match test_passes {
+                            Some(true) => println!("Test passes!!!"),
+                            Some(false) => println!("Test fails!!!"),
+                            None => println!("Nothing was tested..."),
+                        }
+
+                        // println!("{tasks:#?}");
                         match finish_tx.send(()) {
                             Ok(_) => println!("Send finish message!"),
                             Err(err) => println!("Could not send finish message: {err:?}"),
