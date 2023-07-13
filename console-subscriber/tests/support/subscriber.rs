a68130748afbaf9a6915f76627d73f8b4c743b51
Hayden Stainsby <hds@caffeineconcepts.com>
2023-07-13 17:28:50 +0200

    unifying the channels used as barriers
    Created a new struct that manages a broadcast channel to use as various
    pause points in the test run. This ensures that the following things
    happen in order:
    
     - server starts before client connects
     - client connects before test starts
     - test completes before aggregator is dropped

--- console-subscriber/tests/support/subscriber.rs
+++ console-subscriber/tests/support/subscriber.rs
@@ -8,7 +8,10 @@ use console_subscriber::ServerParts;
 use futures::stream::StreamExt;
 use tokio::{
     io::DuplexStream,
-    sync::{broadcast, oneshot},
+    sync::broadcast::{
+        self,
+        error::{RecvError, TryRecvError},
+    },
     task,
 };
 use tonic::transport::{Channel, Endpoint, Server, Uri};
@@ -43,6 +46,99 @@ impl fmt::Display for TestFailure {
     }
 }
 
+#[derive(Clone, Debug, PartialEq, PartialOrd)]
+enum TestStep {
+    Start,
+    ServerStarted,
+    ClientConnected,
+    Completed,
+}
+
+impl fmt::Display for TestStep {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        (self as &dyn fmt::Debug).fmt(f)
+    }
+}
+
+struct TestState {
+    receiver: broadcast::Receiver<TestStep>,
+    sender: broadcast::Sender<TestStep>,
+    step: TestStep,
+}
+
+impl TestState {
+    fn new() -> Self {
+        let (sender, receiver) = broadcast::channel(1);
+        Self {
+            receiver,
+            sender,
+            step: TestStep::Start,
+        }
+    }
+
+    async fn wait_for_step(&mut self, desired_step: TestStep) {
+        loop {
+            if self.step >= desired_step {
+                break;
+            }
+
+            match self.receiver.recv().await {
+                Ok(step) => self.step = step,
+                Err(RecvError::Lagged(_)) => {
+                    // we don't mind being lagged, we'll just get the latest state
+                }
+                Err(RecvError::Closed) => {
+                    panic!("failed to receive current step, waiting for step: {desired_step}, did the test abort?");
+                }
+            }
+        }
+    }
+
+    #[track_caller]
+    fn advance_to_step(&mut self, next_step: TestStep) {
+        loop {
+            match self.receiver.try_recv() {
+                Ok(step) => self.step = step,
+                Err(TryRecvError::Lagged(_)) => {
+                    // we don't mind being lagged, we'll just get the latest state
+                }
+                Err(TryRecvError::Closed) => {
+                    panic!("failed to update current step, did the test abort?")
+                }
+                Err(TryRecvError::Empty) => break,
+            }
+        }
+        if self.step >= next_step {
+            panic!(
+                "cannot advance to a previous or equal step! current step: {current}, next step: {next_step}",
+                current = self.step);
+        }
+
+        match (&self.step, &next_step) {
+            (TestStep::Start, TestStep::ServerStarted) |
+            (TestStep::ServerStarted, TestStep::ClientConnected) |
+            (TestStep::ClientConnected, TestStep::Completed) => {},
+            (_, _) => panic!(
+                "cannot advance more than one step! current step: {current}, next step: {next_step}",
+                current = self.step),
+        }
+
+        self.sender
+            .send(next_step)
+            .expect("failed to send the next test step, did the test abort?");
+    }
+}
+
+impl Clone for TestState {
+    fn clone(&self) -> Self {
+        Self {
+            receiver: self.receiver.resubscribe(),
+            sender: self.sender.clone(),
+            step: self.step.clone(),
+        }
+    }
+}
+
 #[track_caller]
 pub fn assert_tasks<Fut>(expected_tasks: Vec<ExpectedTask>, future: Fut)
 where
@@ -55,9 +151,8 @@ where
 
     let registry = tracing_subscriber::registry().with(console_layer);
 
-    let (finish_tx, mut finish_rx) = broadcast::channel(1);
-
-    let (client_connected_tx, client_connected_rx) = oneshot::channel();
+    let mut test_state = TestState::new();
+    let mut test_state_test = test_state.clone();
 
     let join_handle = thread::Builder::new()
         .name("console_subscriber".into())
@@ -83,7 +178,7 @@ where
             runtime.block_on(async move {
                 task::Builder::new()
                     .name("console-server")
-                    .spawn(console_server(server, server_stream, finish_tx.subscribe()))
+                    .spawn(console_server(server, server_stream, test_state.clone()))
                     .expect("console-test error: could not spawn 'console-server' task");
 
                 let console_client = task::Builder::new()
@@ -94,6 +189,7 @@ where
                         //     .await
                         //     .expect("console-test error: Failure awaiting start signal");
                         // tokio::time::sleep(std::time::Duration::from_millis(200)).await;
+                        test_state.wait_for_step(TestStep::ServerStarted).await;
                         tracing::debug!("#### console-client: after sleep");
 
                         let mut client_stream = Some(client_stream);
@@ -116,9 +212,7 @@ where
                             .await
                             .expect("client-console error: couldn't create client");
                         tracing::debug!("### console-client: before send client connected");
-                        client_connected_tx
-                            .send(())
-                            .expect("console-client error: couldn't send client connected signal");
+                        test_state.advance_to_step(TestStep::ClientConnected);
                         tracing::debug!("### console-client: after send client connected");
 
                         tracing::debug!("#### console-client: before record actual tasks");
@@ -138,9 +232,7 @@ where
                             }
                         }
 
-                        finish_tx
-                            .send(())
-                            .expect("console-test error: failed to send completion message");
+                        test_state.advance_to_step(TestStep::Completed);
 
                         test_result(validation_results)
                     })
@@ -162,9 +254,9 @@ where
         runtime.block_on(async move {
             tracing::debug!("**** Before await client connected...");
 
-            client_connected_rx
-                .await
-                .expect("console-test error: couldn't receive client connected signal");
+            test_state_test
+                .wait_for_step(TestStep::ClientConnected)
+                .await;
 
             tracing::debug!("**** After await client connected...");
 
@@ -175,14 +267,8 @@ where
                 .expect("console-test error: couldn't spawn test task")
                 .await;
 
-            // client_connected_tx
-            //     .send(())
-            //     .expect("console-test error: Could not send start signal");
             tracing::debug!("**** After spawn task away - Before await finish...");
-            match finish_rx.recv().await {
-                Ok(_) => tracing::debug!("finish message received."),
-                Err(err) => tracing::debug!("finish message could not be received: {err}"),
-            }
+            test_state_test.wait_for_step(TestStep::Completed).await;
             tracing::debug!("**** After await finish");
         });
     });
@@ -199,7 +285,7 @@ where
 async fn console_server(
     server: console_subscriber::Server,
     server_stream: DuplexStream,
-    mut completion_rx: broadcast::Receiver<()>,
+    mut test_state: TestState,
 ) {
     let ServerParts {
         instrument_server: service,
@@ -213,11 +299,14 @@ async fn console_server(
         )]))
         .await
         .expect("console subscriber failed.");
+    test_state.advance_to_step(TestStep::ServerStarted);
     tracing::debug!("Waiting for finish signal");
-    match completion_rx.recv().await {
-        Ok(_) => tracing::debug!("Getting ready to drop the aggregate handle."),
-        Err(err) => tracing::debug!("Error waiting for finish signal: {err:?}"),
-    }
+
+    test_state.wait_for_step(TestStep::Completed).await;
+    // match completion_rx.recv().await {
+    //     Ok(_) => tracing::debug!("Getting ready to drop the aggregate handle."),
+    //     Err(err) => tracing::debug!("Error waiting for finish signal: {err:?}"),
+    // }
     drop(aggregate);
 }
 
