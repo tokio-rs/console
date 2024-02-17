b535fdf2b6954cb9abc04b7215e5395571d008d1
hi-rustin <rustin.liu@gmail.com>
2024-02-17 22:27:53 +0800

    feat: connect to update stream
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- console-subscriber/examples/grpc_web/app/src/App.tsx
+++ console-subscriber/examples/grpc_web/app/src/App.tsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { createGrpcWebTransport } from "@connectrpc/connect-web";
+import { createPromiseClient } from "@connectrpc/connect";
+import { Instrument } from "./gen/instrument_connect";
+import { InstrumentRequest } from "./gen/instrument_pb";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const transport = createGrpcWebTransport({
+    baseUrl: "http://localhost:9999",
+  });
+
+  const client = createPromiseClient(Instrument, transport);
+
+  (async () => {
+    try {
+      const updateStream = client.watchUpdates(new InstrumentRequest());
+
+      for await (const value of updateStream) {
+        console.log(value);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  })();
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>gRPC-Web Example</h1>
+      <p>Open the console to see the updates</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
