6cbd6db7b50893c4d4f6b9f090be309fc0e34d8c
Josh McKinney <joshka@users.noreply.github.com>
2024-05-16 01:13:01 -0700

    chore(console): bump ratatui to 0.26.2 and crossterm to 0.27.0 (#515)
    Bumps MSRV to 1.74.0 for all crates.
    
    This necessitated 3 changes to the codebase:
    - `Spans` was renamed to the more ergonomic `Line` type.
    - `Frame` no longer requires a backend type parameter.
    - `Table::new` requires a widths parameter, so we use
      `Table::default().rows(rows)` instead of `Table::new(rows)`.
    
    Crossterm on Windows triggers events for key press as well as release
    and repeat, which causes duplicate key presses. This change filters out
    those events.

--- tokio-console/src/input.rs
+++ tokio-console/src/input.rs
@@ -3,6 +3,18 @@
 // input-handling mechanisms in the background...
 pub use crossterm::event::*;
 
+/// Crossterm on windows reports key release and repeat events which have the
+/// effect of duplicating key presses. This function filters out those events.
+pub fn should_ignore_key_event(input: &Event) -> bool {
+    matches!(
+        input,
+        Event::Key(KeyEvent {
+            kind: KeyEventKind::Release | KeyEventKind::Repeat,
+            ..
+        })
+    )
+}
+
 pub fn should_quit(input: &Event) -> bool {
     use Event::*;
     use KeyCode::*;
@@ -52,4 +64,39 @@ pub(crate) fn is_esc(event: &Event) -> bool {
             ..
         })
     )
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn ignore_key_repeat_and_release_events() {
+        let event = Event::Key(KeyEvent {
+            code: KeyCode::Char('a'),
+            modifiers: KeyModifiers::empty(),
+            kind: KeyEventKind::Press,
+            state: KeyEventState::empty(),
+        });
+
+        assert!(!should_ignore_key_event(&event));
+
+        let event = Event::Key(KeyEvent {
+            code: KeyCode::Char('a'),
+            modifiers: KeyModifiers::empty(),
+            kind: KeyEventKind::Release,
+            state: KeyEventState::empty(),
+        });
+
+        assert!(should_ignore_key_event(&event));
+
+        let event = Event::Key(KeyEvent {
+            code: KeyCode::Char('a'),
+            modifiers: KeyModifiers::empty(),
+            kind: KeyEventKind::Repeat,
+            state: KeyEventState::empty(),
+        });
+
+        assert!(should_ignore_key_event(&event));
+    }
 }
