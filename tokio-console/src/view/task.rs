0f79e653d4e842ce057a5388a42bdaa105ade94f
kvvarun-mecha <varunkkv.mec@gmail.com>
2025-03-21 20:08:38 +0530

    bug: idle line visible after dynamic alloc of layout constraints in task details screen
    I've made the logic to look whether Location is longer than a single viewable length, even before setting constraints both vertically and horizontally. This fixes issue #523

--- tokio-console/src/view/task.rs
+++ tokio-console/src/view/task.rs
@@ -34,7 +34,6 @@ impl TaskView {
     pub(crate) fn update_input(&mut self, _event: input::Event) {
         // TODO :D
     }
-
     pub(crate) fn render(
         &mut self,
         styles: &view::Styles,
@@ -68,65 +67,6 @@ impl TaskView {
             })
             .collect();
 
-        let (
-            controls_area,
-            stats_area,
-            poll_dur_area,
-            scheduled_dur_area,
-            fields_area,
-            warnings_area,
-        ) = if warnings.is_empty() {
-            let chunks = Layout::default()
-                .direction(layout::Direction::Vertical)
-                .constraints(
-                    [
-                        // controls
-                        layout::Constraint::Length(controls.height()),
-                        // task stats
-                        layout::Constraint::Length(10),
-                        // poll duration
-                        layout::Constraint::Length(9),
-                        // scheduled duration
-                        layout::Constraint::Length(9),
-                        // fields
-                        layout::Constraint::Percentage(60),
-                    ]
-                    .as_ref(),
-                )
-                .split(area);
-            (chunks[0], chunks[1], chunks[2], chunks[3], chunks[4], None)
-        } else {
-            let chunks = Layout::default()
-                .direction(layout::Direction::Vertical)
-                .constraints(
-                    [
-                        // controls
-                        layout::Constraint::Length(controls.height()),
-                        // warnings (add 2 for top and bottom borders)
-                        layout::Constraint::Length(warnings.len() as u16 + 2),
-                        // task stats
-                        layout::Constraint::Length(10),
-                        // poll duration
-                        layout::Constraint::Length(9),
-                        // scheduled duration
-                        layout::Constraint::Length(9),
-                        // fields
-                        layout::Constraint::Percentage(60),
-                    ]
-                    .as_ref(),
-                )
-                .split(area);
-
-            (
-                chunks[0],
-                chunks[2],
-                chunks[3],
-                chunks[4],
-                chunks[5],
-                Some(chunks[1]),
-            )
-        };
-
         let stats_area_check = Layout::default()
             .direction(layout::Direction::Horizontal)
             .constraints(
@@ -136,51 +76,161 @@ impl TaskView {
                 ]
                 .as_ref(),
             )
-            .split(stats_area);
+            .split(area);
 
-        let mut location_lines_vector: Vec<String> = vec![];
-        let title = "Location: ";
-        let location_max_width = stats_area_check[0].width as usize - 2 - title.len(); // NOTE: -2 for the border
-        let stats_area = if task.location().len() > location_max_width {
-            let max_width_stats_area = area.width - 45;
-            if max_width_stats_area < task.location().len() as u16 {
-                location_lines_vector = task
-                    .location()
-                    .to_string()
-                    .chars()
-                    .collect::<Vec<char>>()
-                    .chunks(max_width_stats_area as usize)
-                    .map(|chunk| chunk.iter().collect())
-                    .collect();
-
-                let area_needed_to_render_location = task.location().len() as u16;
-                Layout::default()
-                    .direction(layout::Direction::Horizontal)
+        let location_heading = "Location: ";
+        let location_max_width = stats_area_check[0].width as usize - 2 - location_heading.len(); // NOTE: -2 for the border
+        let max_width_stats_area = area.width - 45;
+        let mut location_lines_vector: Vec<String> = task
+            .location()
+            .to_string()
+            .chars()
+            .collect::<Vec<char>>()
+            .chunks(max_width_stats_area as usize)
+            .map(|chunk| chunk.iter().collect())
+            .collect();
+        let no_of_lines_extra_required_to_accomadate_location = location_lines_vector.len() - 1;
+        let (
+            controls_area,
+            stats_area,
+            poll_dur_area,
+            scheduled_dur_area,
+            fields_area,
+            warnings_area,
+        ) = if task.location().len() > location_max_width {
+            if warnings.is_empty() {
+                let chunks = Layout::default()
+                    .direction(layout::Direction::Vertical)
                     .constraints(
                         [
-                            layout::Constraint::Min(area_needed_to_render_location + 15), //Note: 15 is the length of "| Location:     |"
-                            layout::Constraint::Min(32),
+                            // controls
+                            layout::Constraint::Length(controls.height()),
+                            // task stats
+                            layout::Constraint::Length(
+                                10 + no_of_lines_extra_required_to_accomadate_location as u16,
+                            ),
+                            // poll duration
+                            layout::Constraint::Length(9),
+                            // scheduled duration
+                            layout::Constraint::Length(9),
+                            // fields
+                            layout::Constraint::Percentage(60),
                         ]
                         .as_ref(),
                     )
-                    .split(stats_area)
+                    .split(area);
+                (chunks[0], chunks[1], chunks[2], chunks[3], chunks[4], None)
             } else {
-                let area_needed_to_render_location = task.location().len() as u16;
-                location_lines_vector.push(task.location().to_string());
-                Layout::default()
-                    .direction(layout::Direction::Horizontal)
+                let chunks = Layout::default()
+                    .direction(layout::Direction::Vertical)
                     .constraints(
                         [
-                            layout::Constraint::Min(area_needed_to_render_location + 15), //Note: 15 is the length of "| Location:     |"
-                            layout::Constraint::Min(32),
+                            // controls
+                            layout::Constraint::Length(controls.height()),
+                            // warnings (add 2 for top and bottom borders)
+                            layout::Constraint::Length(warnings.len() as u16 + 2),
+                            // task stats
+                            layout::Constraint::Length(
+                                10 + no_of_lines_extra_required_to_accomadate_location as u16,
+                            ),
+                            // poll duration
+                            layout::Constraint::Length(9),
+                            // scheduled duration
+                            layout::Constraint::Length(9),
+                            // fields
+                            layout::Constraint::Percentage(60),
                         ]
                         .as_ref(),
                     )
-                    .split(stats_area)
+                    .split(area);
+
+                (
+                    chunks[0],
+                    chunks[2],
+                    chunks[3],
+                    chunks[4],
+                    chunks[5],
+                    Some(chunks[1]),
+                )
             }
         } else {
-            location_lines_vector.push(task.location().to_string());
-            stats_area_check
+            if warnings.is_empty() {
+                let chunks = Layout::default()
+                    .direction(layout::Direction::Vertical)
+                    .constraints(
+                        [
+                            // controls
+                            layout::Constraint::Length(controls.height()),
+                            // task stats
+                            layout::Constraint::Length(10),
+                            // poll duration
+                            layout::Constraint::Length(9),
+                            // scheduled duration
+                            layout::Constraint::Length(9),
+                            // fields
+                            layout::Constraint::Percentage(60),
+                        ]
+                        .as_ref(),
+                    )
+                    .split(area);
+                (chunks[0], chunks[1], chunks[2], chunks[3], chunks[4], None)
+            } else {
+                let chunks = Layout::default()
+                    .direction(layout::Direction::Vertical)
+                    .constraints(
+                        [
+                            // controls
+                            layout::Constraint::Length(controls.height()),
+                            // warnings (add 2 for top and bottom borders)
+                            layout::Constraint::Length(warnings.len() as u16 + 2),
+                            // task stats
+                            layout::Constraint::Length(10),
+                            // poll duration
+                            layout::Constraint::Length(9),
+                            // scheduled duration
+                            layout::Constraint::Length(9),
+                            // fields
+                            layout::Constraint::Percentage(60),
+                        ]
+                        .as_ref(),
+                    )
+                    .split(area);
+
+                (
+                    chunks[0],
+                    chunks[2],
+                    chunks[3],
+                    chunks[4],
+                    chunks[5],
+                    Some(chunks[1]),
+                )
+            }
+        };
+
+        let stats_area = if location_lines_vector.len() != 1 {
+            let area_needed_to_render_location = task.location().len() as u16;
+            Layout::default()
+                .direction(layout::Direction::Horizontal)
+                .constraints(
+                    [
+                        layout::Constraint::Min(area_needed_to_render_location + 15), //Note: 15 is the length of "| Location:     |"
+                        layout::Constraint::Min(32),
+                    ]
+                    .as_ref(),
+                )
+                .split(stats_area)
+        } else {
+            Layout::default()
+                .direction(layout::Direction::Horizontal)
+                .constraints(
+                    [
+                        layout::Constraint::Percentage(50),
+                        layout::Constraint::Percentage(50),
+                    ]
+                    .as_ref(),
+                )
+                .split(stats_area)
+            // stats_area_check
         };
 
         // Just preallocate capacity for ID, name, target, total, busy, and idle.
@@ -199,7 +249,7 @@ impl TaskView {
 
         let first_line = location_lines_vector[0].clone();
         location_lines_vector.remove(0);
-        let location_vector = vec![bold(title), Span::raw(first_line)];
+        let location_vector = vec![bold(location_heading), Span::raw(first_line)];
         overview.push(Line::from(location_vector));
         for line in location_lines_vector {
             overview.push(Line::from(Span::raw(format!("    {}", line))));
